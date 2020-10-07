b4329228ca9d7345ca33b8f6c60cb7d573595344
Mark <markhimel.cs@gmail.com>
2020-10-07 22:29:28 +0800

    fixed failing specs

--- spec/controllers/project_settings_controller_spec.rb
+++ spec/controllers/project_settings_controller_spec.rb
@@ -2,7 +2,8 @@ require 'rails_helper'
 
 RSpec.describe ProjectSettingsController, type: :controller do
   let(:user) { FactoryBot.create :user, :github, role: :deployer }
-  let!(:setting) { FactoryBot.create :project_setting }
+  let(:team) { FactoryBot.create :team }
+  let(:project) { FactoryBot.create :project, team: team }
 
   before do
     sign_in_as user
@@ -11,15 +12,15 @@ RSpec.describe ProjectSettingsController, type: :controller do
   describe '#edit' do
     before do
       get :edit, params: {
-        team_id: setting.project.team,
-        project_id: setting.project,
-        id: setting.id
+        team_id: team,
+        project_id: project,
+        id: project.settings.id
       }
     end
 
     it 'assigns team and project' do
-      expect(assigns(:team)).to eq(setting.project.team)
-      expect(assigns(:project)).to eq(setting.project)
+      expect(assigns(:team)).to eq(team)
+      expect(assigns(:project)).to eq(project)
     end
 
     it 'renders edit page' do
@@ -30,17 +31,17 @@ RSpec.describe ProjectSettingsController, type: :controller do
   describe '#update' do
     let(:common_params) do
       {
-        team_id: setting.project.team,
-        project_id: setting.project,
-        id: setting
+        team_id: team.id,
+        project_id: project.id,
+        id: project.settings.id
       }
     end
     let(:valid_params) do
-      { project_setting: { git_repo_url: 'https://github.com/updated' } }
+      { project_setting: { git_repo_url: 'https://github.com/updated', jira_site: 'https://atlassian.net' } }
     end
 
     let(:invalid_params) do
-      { project_setting: { git_repo_url: 'invalid_url' } }
+      { project_setting: { git_repo_url: 'invalid_url', jira_site: 'invalid_url' } }
     end
 
     context 'when parameters are valid' do
@@ -48,14 +49,14 @@ RSpec.describe ProjectSettingsController, type: :controller do
 
       it 'successfully updates the attributes' do
         expect { subject }.
-          to change { setting.reload.git_repo_url }.
-          from('https://github.com').to('https://github.com/updated')
+          to change { project.settings.reload.git_repo_url }.
+          from(nil).to('https://github.com/updated')
       end
 
       it 'updates and redirects to edit page again' do
         subject
         expect(response).
-          to redirect_to edit_team_project_project_settings_path(setting.project.team, setting.project)
+          to redirect_to edit_team_project_project_settings_path(team, project)
       end
     end
 
@@ -63,7 +64,7 @@ RSpec.describe ProjectSettingsController, type: :controller do
       subject { patch :update, params: common_params.merge(invalid_params) }
 
       it 'does not update any attribute' do
-        expect { subject }.to_not change { setting.reload.git_repo_url }
+        expect { subject }.to_not change { project.settings.reload.git_repo_url }
       end
 
       it 'fails update and renders edit page' do
